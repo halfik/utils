278691d8799d6e64ed4169c217e881e10a894b36
Kamil Pietrzak <kamil.pietrzak@netinteractive.pl>
2014-12-01 11:29:12 +0100

    bind app

--- src/Netinteractive/Utils/UtilsServiceProvider.php
+++ src/Netinteractive/Utils/UtilsServiceProvider.php
@@ -1,43 +1,55 @@
-<?php namespace Netinteractive\Utils;
+<?php
+
+namespace Netinteractive\Utils;
+
 use Illuminate\Support\ServiceProvider;
 
-class UtilsServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+class UtilsServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('netinteractive/utils');
-	}
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('netinteractive/utils');
+    }
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        App::bind('Utils', function($app)
+        {
+            return new \Netinteractive\Utils\Utils;
+        });
+    }
+
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
 
 }
